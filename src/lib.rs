b6deae9f9c9265aa17d0d7fb6f29672dc8e30866
Anton Sapunov <fort.sav.28@gmail.com>
2020-04-30 01:27:14 +0700

    Some bad error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,28 @@
 use teloxide::prelude::*;
 // TODO error handling with ParseError
 use url::Url;
+use std::error;
 
 // TODO remove all panics!
-pub fn handle_message(message: &str) -> String {
-    let parsed_link = Url::parse(message).expect("Parsing Error!");
+pub fn handle_message(message: &str) -> Result<String, Box<dyn error::Error>> {
+    let parsed_link = Url::parse(message)?;
 
     dbg!(&parsed_link.as_str());
 
     let mut path_segments = parsed_link.path_segments()
         .ok_or_else(|| "cannot be base")
-        .expect("Error while find filename")
+        ?
         .skip(1); // Skip "photo"
-    let filename = path_segments.next().expect("Error while getting filename");
+    let filename = path_segments.next().ok_or_else(|| "Error while getting filename")?;
 
     // Remove vp9 and av1 from filename if contains
     let result_filename = filename.replace("vp9", "");
     let result_filename = result_filename.replace("av1", "");
 
     dbg!(&result_filename);
-    let result_link = parsed_link.join(&result_filename).expect("Error while join");
+    let result_link = parsed_link.join(&result_filename)?;
     dbg!(&result_link.as_str());
-    result_link.into_string()
+    Ok(result_link.into_string())
 }
 
 pub async fn run() {
@@ -33,9 +34,11 @@ teloxide::enable_logging!();
     Dispatcher::new(bot)
         .messages_handler(|rx: DispatcherHandlerRx<Message>| {
             rx.for_each(|message| async move {
-                let link = &message.update.text().expect("Faild while read link");
-                dbg!(&link);
-                let respond = handle_message(link);
+                let link = &message.update.text()
+                    .unwrap_or("This is not a text dude");
+
+                let respond = handle_message(link)
+                    .unwrap_or("Sorry, I can`t handle this message".to_owned());
                 message.answer(&respond).send().await.log_on_error().await;
 
             })
