8c82629eafdf49fe5c85e59a38abdd8e8a4bceca
Anton Sapunov <fort.sav.28@gmail.com>
2020-09-30 20:08:25 +0300

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::path::PathBuf;
 use teloxide::prelude::*;
 
+type TeloxideMessage = teloxide::dispatching::DispatcherHandlerCx<Message>;
+
 mod handle_message;
 mod video;
 
@@ -15,45 +17,70 @@ pub async fn run() {
             rx.for_each_concurrent(None, |message| async move {
                 match &message.update.text() {
                     Some(link) => {
-                        // TODO delete fucking unwrap
-                        let wait_message = message.answer("OK I got it, wait a little").send().await.unwrap();
-
-                        match video::Video::new(&link).await {
-                        Ok(video) => {
-                            let path_to_result = PathBuf::from(&video.filename);
-                            message
-                                .answer_video(teloxide::types::InputFile::File(path_to_result))
-                                .send()
-                                .await
-                                .log_on_error()
-                                .await;
-                        }
-                        Err(err) => {
-                            message
-                                .answer(
-                                    "Probably your message is not a valid url to 9gag video, I can't handle it:\n".to_owned() + &err.to_string(),
-                                )
-                                .send()
-                                .await
-                                .log_on_error()
-                                .await;
-                        }
+                        answer_to_user(&message, link).await;
                     }
-
-                        // Delete message "wait pls"
-                        message.delete_message().message_id(wait_message.id).send().await.log_on_error().await;
-                    },
                     None => {
-                        message
-                            .answer("Your message is not plain text, I can`t handle it.")
-                            .send()
-                            .await
-                            .log_on_error()
-                            .await;
+                        not_plain_text(&message).await;
                     }
                 }
             })
         })
         .dispatch()
+        .await;
+}
+
+async fn answer_to_user(message: &TeloxideMessage, link: &str) {
+    if let Ok(wait_message) = message.answer("OK, I got it, wait a little").send().await {
+        match video::Video::new(&link).await {
+            Ok(video) => {
+                send_video_to_user(&video, &message).await;
+            }
+            Err(err) => {
+                send_err_to_user(&err, &message).await;
+            }
+        }
+        delete_wait_message(&message, &wait_message).await;
+    }
+}
+
+async fn not_plain_text(message: &TeloxideMessage) {
+    message
+        .answer("Your message is not plain text, I can`t handle it")
+        .send()
+        .await
+        .log_on_error()
+        .await;
+}
+
+async fn send_video_to_user(video: &video::Video, message: &TeloxideMessage) {
+    let path_to_result = PathBuf::from(&video.filename);
+    message
+        .answer_video(teloxide::types::InputFile::File(path_to_result))
+        .send()
+        .await
+        .log_on_error()
+        .await;
+}
+
+async fn send_err_to_user(err: &anyhow::Error, message: &TeloxideMessage) {
+    message
+        .answer(
+            "Probably your message is not a valid url to 9gag video, I can't handle it:\n"
+                .to_owned()
+                + &err.to_string(),
+        )
+        .send()
+        .await
+        .log_on_error()
+        .await;
+}
+
+async fn delete_wait_message(message: &TeloxideMessage, wait_message: &Message) {
+    message
+        .delete_message()
+        .message_id(wait_message.id)
+        .send()
+        .await
+        .log_on_error()
         .await;
 }
